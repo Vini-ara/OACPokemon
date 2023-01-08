c071e047942e3af448887eaf9e9ba9b02713d010
vinicius araujo <vinicius.ar02@gmail.com>
2023-01-08 01:32:24 -0300

    refatorei um bocado mas ainda tem uns bugs

--- main.s
+++ main.s
@@ -1,20 +1,20 @@
 .eqv T_OBJ 8 #tamanho de cada objeto
 .data
-.include "mapa.data"
-.include "mapa2.data"
-.include "imagens/house0.data"
+.include "mapas/mapa.data"
+.include "mapas/mapa2.data"
+.include "sprites/house0.data"
 
 .text
 #iniciando mapa
 SETUP:	
-  call inicia_objetos
+  jal ra, inicia_objetos
 
 	li s0,30 #linha
 	li s1,30 #coluna
 
 	mv a0,s0
 	mv a1,s1
-	call CARREGA_MAPA
+	jal ra, CARREGA_MAPA
 	
 GAME_LOOP:
 	li a0,0
@@ -27,6 +27,7 @@ GAME_LOOP:
 	mv a1,s1 #coluna
 	jal ra,MOVE
 
+
 	jal zero,GAME_LOOP
 
 # -CARREGA_MAPA
@@ -35,98 +36,79 @@ GAME_LOOP:
 #     > ao = linha do jogador
 #     > a1 = coluna do jogador
 CARREGA_MAPA:
+  addi sp, sp, -28
+  sw ra, 24(sp)
+  sw t5, 20(sp)
+  sw t4, 16(sp)
+  sw t3, 12(sp)
+  sw t2, 8(sp)
+  sw t1, 4(sp)
+  sw t0, 0(sp)
+
 	mv t0,a1 # t0 = coluna do jogador
 
 	addi a1,a0,-7 #ajusta a0(linha) para a primeira linha do display   
 	addi a2,t0,-10 # ajusta a1(coluna) para a primeira coluna do display
 	la a0,mapa2 # endereco da matriz do mapa
-	li a3,1 # offset da funcao AJUSTA_XY
+	li a3,1 # .data do mapa em byte
 
-	addi sp,sp,-4
-	sw ra,0(sp)
 	call AJUSTA_XY # retorna o endereco do mapa para printar somente a parte em que o jogador esta
-	lw ra,0(sp)
-	addi sp,sp,4
 
-	mv a2,a0  
-	li a0,0 #a0 = contador coluna
-	li a1,0 #a1 = contador linha
-	la a3,OBJETOS #a3 = comecco do vetor objetos
+	li t0,0 #a0 = contador coluna
+	li t1,0 #a1 = contador linha
+	mv t2,a0  #t2 = endereco do .data
+	la t3,OBJETOS #a3 = comecco do vetor objetos
 
 LOOP_CARREGA_MAPA:
-	lb t0,0(a2) # recebe o byte da matriz do mapa
-	addi t1,zero,T_OBJ # tamanho de cada objeto(VAI MUDAR)
-	mul t1,t1,t0 # quantidade de bytes que serao adicionados ao endereco objetos
-	add t1,a3,t1 # t1 recebe o endereco do objeto requisitado
-	lw t0,0(t1) # t0 recebe o endereco do sprite do objeto
-
-	#aqui é colocado os argumentos e ra na pilha
-	addi sp,sp,-20
-  sw ra,16(sp)
-  sw a0,12(sp)
-  sw a1,8(sp)
-  sw a2,4(sp)
-  sw a3,0(sp)
+	lb t4,0(t2) # t4 recebe o byte da matriz do mapa (indice)
+	addi t5,zero,T_OBJ # t5 = tamanho de cada objeto(VAI MUDAR)
+	mul t5,t5,t4 # quantidade de bytes que serao adicionados ao endereco objetos
+	add t5,t3,t5 # t5 recebe o endereco do objeto requisitado
+	lw t4,0(t5) # t0 recebe o endereco do sprite do objeto
 	
 	#ajustando argumentos para funcao print
-	li t2, 16
-	mv t3,a1
-	mul a1,t2,a0 #a1 = coluna
-	mul a2,t2,t3 #a2 = linha
-	mv a0,t0 #a0 = endereco do sprite
+	mv a0,t4 #a0 = endereco do sprite
+	li t5, 16
+	mul a1,t5,t0 #a1 = coluna
+	mul a2,t5,t1 #a2 = linha
 	li a3,0
-	call DRAW_IMAGE
+	jal ra, DRAW_IMAGE
 	
-	lw a3,0(sp)
-  lw a2,4(sp)
-  lw a1,8(sp)
-  lw a0,12(sp)
-  lw ra,16(sp)
-  addi sp,sp,20
+	li t5,20     # tamanho da tela 
+	addi t2,t2,1 # incrementa endereco do mapa
+	addi t0,t0,1 # adiciona 1 ao contador coluna
+	blt t0,t5,LOOP_CARREGA_MAPA # verifica se acabaram as colunas
+
+	la t5,mapa2 
+	lw t5,0(t5) # largura do mapa
+	addi t2,t2,-20 
+	add t2,t2,t5 # vai para a proxima linha
+
+	li t0,0 # reinicia o contador de colunas
+	addi t1,t1,1  # incrementa o contador de linhas
+	li t5,15 
+	blt t1,t5,LOOP_CARREGA_MAPA # verifica se acabaram as linhas
 	
-	li t0,20
-	addi a2,a2,1
-	addi a0,a0,1 #adiciona 1 ao contador coluna
-	blt a0,t0,LOOP_CARREGA_MAPA
-	la t0,mapa2
-	lw t0,0(t0)
-	addi a2,a2,-20
-	add a2,a2,t0
-	li a0,0
-	addi a1,a1,1
-	li t0,15
-	blt a1,t0,LOOP_CARREGA_MAPA
-	
-	addi sp,sp,-20
-	sw ra,16(sp)
-	sw a0,12(sp)
-	sw a1,8(sp)
-	sw a2,4(sp)
-	sw a3,0(sp)
-	
-	la a0,house0 #suposto personagem
-	li a1,160
-	li a2,112
-	li a3,0
-	call DRAW_IMAGE
-	lw a3,0(sp)
-	lw a2,4(sp)
-	lw a1,8(sp)
-	lw a0,12(sp)
-	lw ra,16(sp)
-	addi sp,sp,20
-	
+  lw t0, 0(sp)
+  lw t1, 4(sp)
+  lw t2, 8(sp)
+  lw t3, 12(sp)
+  lw t4, 16(sp)
+  lw t5, 20(sp)
+  lw ra, 24(sp)
+  addi sp, sp, 28
+
 	ret
 
 #retorna em a0 a tecla usada	
 KEY2:	li t1,0xFF200000		# carrega o endereço de controle do KDMMIO
 	lw t0,0(t1)			# Le bit de Controle Teclado
 	andi t0,t0,0x0001		# mascara o bit menos significativo
-   	beq t0,zero,FIM   	   	# Se não há tecla pressionada então vai para FIM
-  	lw a0,4(t1)  			# le o valor da tecla tecla
-	#sw t2,12(t1)  			# escreve a tecla pressionada no display
+  beq t0,zero,FIM   	   	# Se não há tecla pressionada então vai para FIM
+  lw a0,4(t1)  			# le o valor da tecla tecla
 FIM:	ret				# retorna
 	
+
 # --- AJUSTA_XY 
 #  - Retorna o endereco de um objeto em um .data, dadas suas coordenadas
 #     Parametros:
@@ -137,6 +119,12 @@ FIM:	ret				# retorna
 #     Retorno: 
 #     > a0 = endereco do .data em que o objeto esta
 AJUSTA_XY:
+  addi sp, sp, -16
+  sw ra, 12(sp)
+  sw t2, 8(sp)
+  sw t1, 4(sp)
+  sw t0, 0(sp)
+
 	lw t0,0(a0) #t0 = tamanho X do .data
 
 	mul t1,t0,a1 #t1 = t0 (largura do .data) * a2 (linha)
@@ -147,9 +135,16 @@ AJUSTA_XY:
 	add a0,a0,t1 #a0 = a0 (endereco do .data) + t1 (o Y do meu objeto)
 	add a0,a0,t2 #a0 = a0 + t2 (o X do meu objeto)
 
-	addi a0,a0,8 #corrige um offset, mas isso n era p estar aqui (n tem relacao direta com a funcao)!!
+	addi a0,a0,8 #corrige as duas words do tamanho do .data
+
+  lw t0, 0(sp)
+  lw t1, 4(sp)
+  lw t2, 8(sp)
+  lw ra, 12(sp)
+  addi sp, sp, 16
 	
 	ret
+
 
 .include "print.s"
 .include "objetos.s"
--- move.s
+++ move.s
@@ -1,69 +1,82 @@
 .text 
-#Movimenta o persogem pelo mapa
-#argumentos:
-# a0 = linha atual
-# a1 = coluna atual
-# a3 = tecla pressionada
+# MOVE
+#   -Movimenta o persogem pelo mapa
+#     -argumentos:
+#        >a0 = linha atual
+#        >a1 = coluna atual
+#        >a3 = tecla pressionada
+
 MOVE:
-	li t0,'w'
-	beq a3,t0,MOVE_CIMA
-	li t0,'a'
-	beq a3,t0,MOVE_ESQ
-	li t0,'s'
-	beq a3,t0,MOVE_BAIXO
-	li t0,'d'
-	beq a3,t0,MOVE_DIR
+  addi sp, sp, -16
+  sw ra, 12(sp)
+  sw t2, 8(sp)
+  sw t1, 4(sp)
+  sw t0, 0(sp)
+
+  mv t0, a0
+  mv t1, a1
+
+	li t2,'w'
+	beq a3,t2,MOVE_CIMA
+	li t2,'a'
+	beq a3,t2,MOVE_ESQ
+	li t2,'s'
+	beq a3,t2,MOVE_BAIXO
+	li t2,'d'
+	beq a3,t2,MOVE_DIR
 	ret
 	
-	
 VOLTA_MOVE:	
-	addi sp,sp,-12
-	sw ra,8(sp)
-	sw a1,4(sp)
-	sw a0,0(sp)
-	call TILE_ANDAVEL
-	mv t0,a0
-	lw a0,0(sp)
-	lw a1,4(sp)
-	lw ra,8(sp)
-	addi sp,sp,12
-	beqz t0,FIM_MOVE
-	mv s0,a0
-	mv s1,a1
-	addi sp,sp,-4
-	sw ra,0(sp)
-	call CARREGA_MAPA
-	lw ra,0(sp)
-	addi sp,sp,4
-	
-	
+	jal ra, TILE_ANDAVEL
+
+	beqz a0,FIM_MOVE
+
+	mv s0,t0
+	mv s1,t1
+
+	mv a0,s0
+	mv a1,s1
+	jal ra, CARREGA_MAPA
+
+	la a0,house0 #suposto personagem
+	li a1,160
+	li a2,112 ### (TO DO) Desse jeito ele comeca a ser printado pela cabeca nessa posicao, mas essa eh a posicao do pe dele
+	li a3,0
+	jal ra, DRAW_IMAGE
+
 	
 FIM_MOVE:
+  lw t0, 0(sp)
+  lw t1, 4(sp)
+  lw t2, 8(sp)
+  lw ra, 12(sp)
+  addi sp, sp, 16
+
 	ret
 	
 MOVE_CIMA:
-	beqz a0,FIM_MOVE
-	addi a0,a0,-1
-	j VOLTA_MOVE
+	beqz t0,FIM_MOVE
+	addi t0,t0,-1
+	jal zero, VOLTA_MOVE
 	
 MOVE_ESQ:
-	beqz a1,FIM_MOVE
-	addi a1,a1,-1
-	j VOLTA_MOVE
+	beqz t1,FIM_MOVE
+	addi t1,t1,-1
+	jal zero, VOLTA_MOVE
 	
 MOVE_BAIXO:
-	la t0,mapa
-	lw t0,4(t0)
-	beq a0,t0,FIM_MOVE
-	addi a0,a0,1
-	j VOLTA_MOVE
+	la t2,mapa2
+	lw t2,4(t2)
+	beq t0,t2,FIM_MOVE
+	addi t0,t0,1
+	jal zero, VOLTA_MOVE
 	
 MOVE_DIR:
-	la t0,mapa
-	lw t0,0(t0)
-	beq t0,a1,FIM_MOVE
-	addi a1,a1,1
-	j VOLTA_MOVE
+	la t2,mapa2
+	lw t2,0(t2)
+	beq t2,t1,FIM_MOVE
+	addi t1,t1,1
+	jal zero, VOLTA_MOVE
 
 # TILE_ANDAVEL
 #   - checa se o tile eh andavel
@@ -73,17 +86,21 @@ MOVE_DIR:
 #   Retorno: 
 #     > a0 = andavel ? 1 : 0
 TILE_ANDAVEL:
+	addi sp,sp,-16
+	sw ra,12(sp)
+  sw t2,8(sp)
+	sw t1,4(sp)
+	sw t0,0(sp)
+
 	mv t0,a0
 	mv t1,a1
-	la a0,mapa
+
+	la a0,mapa2
 	mv a1,t0
 	mv a2,t1
 	li a3,1
-	addi sp,sp,-4
-	sw ra,0(sp)
-	call AJUSTA_XY
-	lw ra,0(sp)
-	addi sp,sp,4
+	jal ra, AJUSTA_XY
+
 	lb t0,0(a0) # t0 = codigo do objeto
 	la t2,OBJETOS
 	
@@ -91,4 +108,11 @@ TILE_ANDAVEL:
 	mul t1,t1,t0 #quantidade de bytes que serao adicionados ao endereco objetos
 	add t1,t2,t1 #t1 recebe o endereco do objeto requisitado
 	lw a0,4(t1)
+
+	lw t0,0(sp)
+	lw t1,4(sp)
+  lw t2,8(sp)
+	lw ra,12(sp)
+	addi sp,sp,16
+
 	ret
