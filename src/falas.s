e6ee7bc9573fd8514499fadc995b9806ee6ff6ea
Daniel <64702639+dancpluz@users.noreply.github.com>
2023-02-14 17:39:56 -0300

    Data das falas do jogo

---
+++ src/falas.s
@@ -0,0 +1,132 @@
+.data
+    
+# Falas dos personagens
+# "NOME" + n (n par = linha cima / n ímpar = linha baixo)
+
+# INÍCIO DO JOGO
+    
+    START0: .string "Bem vindo ao terceiro semestre"
+    START1: .string "calouro, depois de fugir da ISCocia,"
+
+    START2: .string "agora voce embarcara na cidade"
+    START3: .string "de OACapulco!"
+
+    START4: .string "Eu sou o professor Lamar,"
+    START5: .string "como voce deve se lembrar."
+
+    START6: .string "Siga o que eu digo e nao fale de"
+    START7: .string "python que nao vai ter problemas."
+
+    START8: .string "Sinto lhe dizer, mas voce esta preso"
+    START9: .string "nessa cidade maravilhosa e so existe"
+
+    START10: .string "uma forma de sair daqui, o 110!"
+
+    START12: .string "Passa no LINF que vou te explicar"
+    START13: .string "melhor."
+
+# LABORATÓRIO
+
+    LAMAR0: .string "Antes de começarmos..."
+
+    LAMAR2: .string "Ja fez o testinho hoje?"
+
+    LAMAR4: .string "Bom, preciso te falar que nessa"
+    LAMAR5: .string "cidade existem varios"
+
+    LAMAR6: .string "bichos nada sanitarios."
+
+    LAMAR8: .string "Aqui eles sao chamados de pokemons,"
+    LAMAR9: .string "vou te ensinar como treina-los para"
+
+    LAMAR10: .string "batalhar e como fugir do IBAMA."
+
+    LAMAR12: .string "Chegou o grande momento!"
+
+    LAMAR14: .string "Escolha o seu fiel companheiro"
+    LAMAR15: .string "nessa aventura!"
+
+    LAMAR16: .string "Ignore o cheiro por favor."
+
+    # aparece a opção de escolher entre os 5 pokemons
+    # jogador escolhe
+
+    LAMAR18: .string "Otima escolha! Boa sorte na sua"
+    LAMAR19: .string "aventura e lembre-se:"
+
+    LAMAR20: .string "volte aqui quando estiver pronto"
+    LAMAR21: .string "para comprar o passe do 110. "
+
+    LAMAR22: .string "Voce tambem pode passar no RU"
+    LAMAR23: .string "para alimentar seu pokemon."
+
+    # jogador fala com o lamar depois
+    # se o jogador não tiver dinheiro pro passe
+
+    LAMAR24: .string "Calouro voce ainda nao conseguiu os"
+    LAMAR25: .string "creditos para o passe."
+
+    LAMAR26: .string "Vai batalhar ou arranje dinheiro com"
+    LAMAR27: .string "um agiota."
+
+    # se o jogador tiver dinheiro pro passe
+
+    LAMAR28: .string "Parabens calouro, agora chegou seu"
+    LAMAR29: .string "momento!"
+
+    LAMAR30: .string "Pegue esse passe, va para o 110 e"
+    LAMAR31: .string "conquiste sua liberdade!"
+
+# GINÁSIO
+
+    # jogador tenta entrar no ginásio
+    # se ele não possuir o passe:
+
+    GIN0: .string "Opa opa calouro, nao ficou sabendo?"
+    GIN1: .string "Voce precisa do passe pra entrar!"
+
+    # se ele possuir o passe:
+    # jogador entra normalmente
+    # jogador fala com o cobrador
+
+    GIN2: .string "O que um calouro idiota esta fazendo"
+    GIN3: .string "aqui no 110?"
+
+    GIN4: .string "Se voce acha que vou te deixar sair"
+    GIN5: .string "daqui, voce esta muito enganado!"
+
+    GIN6: .string "Prepare-se para ser derrotado!"
+
+    GIN8: .string "Tem uma moeda de 50?..."
+
+    # a luta começa
+    # se o jogador ganhar
+
+    GIN10: .string "Isso nao pode ser possivel!"
+
+    GIN12: .string "Tudo bem garoto, pode passar"
+    GIN13: .string "pela catraca."
+
+    # o jogo acaba
+    # se o jogador perder
+
+    GIN14: .string "HAHAHAHA voce realmente achou"
+    GIN15: .string "que poderia me derrotar?!"
+
+    GIN16: .string "Calouro estupido, como de costume,"
+    GIN17: .string "passando vergonha."
+
+    # o jogador volta para a cidade sem o passe
+
+# HOSPITAL
+
+    # jogador fala com a tia do RU
+
+    CURA0: .string "Bem vindo ao RU, hoje temos isca de"
+    CURA1: .string "carne, qual sua matricula?"
+
+# LOJA
+    # jogador fala com o vendedor
+
+    LOJA0: .string "Eae campeao, vai uma marmita?"
+    LOJA1: .string "Te vendo essa por 50. Aceito pix."
